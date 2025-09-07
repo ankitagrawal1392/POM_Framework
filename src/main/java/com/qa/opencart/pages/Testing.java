d7345de64e304a6e05c9a684dc758ffc51b2da32
ankitagrawal1392 <ankitagrawal1392@gmail.com>
2025-09-08 00:50:20 +0530

    Update Testing.java

--- src/main/java/com/qa/opencart/pages/Testing.java
+++ src/main/java/com/qa/opencart/pages/Testing.java
@@ -5,7 +5,8 @@ public class Testing {
 	public static void main(String[] args) {
 		
 		 String sb = "Selenium";
-	   String tv = "Testing";
+	     String tv = "Testing";
+		 String gv = "Automation"
 
 	}
 
