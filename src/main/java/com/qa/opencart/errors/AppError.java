c12e7577b3e6b012e93f393e86a7d3619a3252f2
Ankit Agrawal <ankit.agrawal1@ibm.com>
2025-10-03 20:17:05 +0530

    App error modified

--- src/main/java/com/qa/opencart/errors/AppError.java
+++ src/main/java/com/qa/opencart/errors/AppError.java
@@ -2,6 +2,6 @@ package com.qa.opencart.errors;
 
 public class AppError {
 	
-	public static final String INVALID_BROWSER_MESG  = "Please pass te right browser: only chrome/edge/fiefox/safari is allowed";
+	public static final String INVALID_BROWSER_MESG  = "Please pass te right browser: only chrome/edge/fiefox/safari browsers is allowed";
 
 }
